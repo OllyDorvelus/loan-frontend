b02b6b474dbbdfaa7b9a53d43d33ce368299947d
Olivier Dorvelus <ollydorvelus@gmail.com>
2022-03-26 15:37:05 -0400

    add layout component

---
+++ components/Layout.tsx
@@ -0,0 +1,14 @@
+import React, { ReactElement } from 'react'
+import Head from 'next/head';
+
+type Props = {
+    title: String,
+}
+
+export default function Layout(props: Props): ReactElement {
+    return (
+        <Head>
+            <title>{props.title}</title>
+        </Head>
+    )
+}
